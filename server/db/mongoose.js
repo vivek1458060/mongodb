b34f93622774308b70b69122f6c6f154a7c048e3
unknown <vivek1458060@gmail.com>
2018-01-07 23:11:18 +0530

    =

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds245357.mlab.com:45357/db235733'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds245357.mlab.com:45357/db235733');
 
 module.exports = {mongoose};
