658695af4305f9c763f675a859fbebe4b8aa4239
unknown <vivek1458060@gmail.com>
2018-01-08 21:37:11 +0530

    =test DELETE /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,39 @@ describe('GET  /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', (done) => {
+  it('should remove a todo',(done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res) => {
+      if(err) {
+        return done(err);
+      }
+    Todo.findById(hexId).then((todo) => { //query database using findById toNotExist
+      expect(todo).toNotExist();
+      done();
+    }).catch((e) => done(e));
+
+    })
+  });
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
